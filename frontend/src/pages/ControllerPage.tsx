4ce46e5c71dd4ddc25d802bbbb0e98aed09f89dc
Ned Palacios <npdoesmc@gmail.com>
2025-09-06 20:54:14 +0800

    feat: change background

--- frontend/src/pages/ControllerPage.tsx
+++ frontend/src/pages/ControllerPage.tsx
@@ -351,7 +351,7 @@ function ControllerPageContent() {
           left: 0,
           width: "100%",
           height: "100%",
-          backgroundImage: 'url("https://picsum.photos/1280/720")',
+          backgroundImage: 'url("https://images.unsplash.com/photo-1545569341-9eb8b30979d9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
